7bbad95484f33065d689795b730af9df42dd5a64
Zhang Jikun <39144313+zjk2606@users.noreply.github.com>
2021-06-21 04:40:27 +0800

    21/06/21

---
+++ Codeforces/1515E/solution.cpp
@@ -0,0 +1,139 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vii = vector<pii>;
+using vll = vector<pll>;
+#define fi first
+#define se second
+#define sz(c)  ((int)(c).size())
+#define all(c) (c).begin(), (c).end()
+#define forn(i, m, n)  for (int i = m, nnnn = (n); i < nnnn; ++i)
+#define pb  push_back
+#define mp  make_pair
+#define NIL 0
+#define INF INT_MAX
+#define int128 __int128_t
+
+ll gcd(ll a, ll b) {
+    return b ? gcd (b, a % b) : a;
+}
+
+ll lcm(ll a, ll b) {
+    return a / gcd(a, b) * b;
+}
+
+vector<int> sieve;
+void calc_sieve(int n){
+    sieve.reserve(n+1);
+    forn(i, 0, n+1) sieve[i] = i;
+    forn(i, 2, n+1){
+        if (sieve[i] == i){
+            forn(j, 2, n/i+1){
+                if (sieve[i*j] == i*j) sieve[i*j] = i;
+            }
+        }
+    }
+}
+
+ll norm(ll a, ll P) {
+    while (a >= P) a -= P;
+    while (a < 0) a += P;
+    return a;
+}
+
+ll mul_mod(ll a, ll b, ll P) {
+    return a * b % P;
+}
+
+ll pow_mod(ll a, ll b, ll P){
+    ll curr = a, ans = 1;
+    while(b != 0){
+        if (b&1) {
+            ans *= curr;
+            ans%=P;
+        }
+        curr *= curr;
+        curr %= P;
+        b >>= 1;
+    }
+    return ans;
+}
+
+ll inv_mod(ll a, ll P) {
+    return pow_mod(a, P - 2ll, P);
+}
+
+ll div_mod(ll a, ll b, ll P) {
+    return a * inv_mod(b, P) % P;
+}
+
+int msb(ll a){ 
+    for(int i = 62; i >= 0; i--){
+        if (a & ((ll)1 << i)) return i + 1;
+    }
+    return 0;
+}
+
+vector<ll> fact;
+void calc_fact(int n, ll P){
+    if (fact.size() < 1) fact.pb(1);
+    forn(i, fact.size(), n+1) fact.pb(fact[i-1] * i  % P);
+}
+
+vector<ll> fact_inv;
+void calc_fact_inv(int n, ll P){
+    if (fact_inv.size() < 1) fact_inv.pb(1);
+    forn(i, fact_inv.size(), n+1) fact_inv.pb(fact_inv[i-1] * inv_mod(i, P) % P);
+}
+
+ll nCr(int n, int r, ll P){
+    if (r > n) return 0;
+    calc_fact(n, P);
+    calc_fact_inv(n, P);
+    return (fact[n] * fact_inv[r] % P) * fact_inv[n-r] % P;
+}
+
+ll nCr_inv(int n, int r, ll P){
+    if (r > n) return 0;
+    calc_fact(n, P);
+    calc_fact_inv(n, P);
+    return (fact_inv[n] * fact[r] % P) * fact[n-r] % P;
+}
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+ll mod = 1000000007;
+
+void solve(){
+	ll n, ans = 0;
+    cin >> n >> mod;
+    ll p2[n+1];
+    p2[0] = 1;
+    forn(i, 1, n+1) p2[i] = p2[i-1]*2%mod;
+    ll dp[n+1][n+1];
+    memset(dp, 0, sizeof dp);
+    forn(i, 1, n+1){
+        forn(j, 1, i+1){
+            if (j == i) dp[i][j] = p2[i-1];
+            else {
+                forn(k, 1, j+1){
+                    dp[i][j] += dp[i-k-1][j-k]*nCr(j, j-k, mod) % mod * p2[k-1];
+                    dp[i][j] %= mod;
+                }
+            }
+        }
+    }
+    forn(i, 0, n+1) ans += dp[n][i], ans %= mod;
+    cout << ans;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << setprecision(16) << fixed;
+    solve();
+}
