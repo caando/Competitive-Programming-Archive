97a91ad453c0534400731b8750705d6dfffd6c93
Zhang Jikun <39144313+zjk2606@users.noreply.github.com>
2021-10-20 04:17:21 +0800

    CSES grind (Sorting and Searching)

---
+++ CSES/Sorting and Searching/Josephus Problem II/solution.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vii = vector<pii>;
+using vll = vector<pll>;
+#define fi first
+#define se second
+#define sz(c)  ((int)(c).size())
+#define all(c) (c).begin(), (c).end()
+#define forn(i, m, n)  for (int i = m, nnnn = (n); i < nnnn; ++i)
+#define pb  push_back
+#define mp  make_pair
+#define NIL 0
+#define INF INT_MAX
+#define int128 __int128_t
+    
+void debug_out() { cerr << endl; }
+    
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cerr << " " << to_string(H);
+    debug_out(T...);
+}
+    
+#ifdef LOCAL
+    #define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+    #define debug(...) 42
+#endif
+    
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+const long double eps = 1e-9;
+const long long mod = 1000000007;
+const int MAXN = 1000000;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+
+void solve(){
+    ll n, k;
+    cin >> n >> k;
+    ordered_set s;
+    forn(i, 0, n) s.insert(i+1);
+    ll cur = 0;
+    while(!s.empty()){
+        cur += k;
+        cur%=sz(s);
+        cout << (*s.find_by_order(cur)) << ' ';
+        s.erase(s.find_by_order(cur));
+    }
+}
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << setprecision(12) << fixed;
+    solve();
+}
