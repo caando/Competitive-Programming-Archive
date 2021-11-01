293dac08941a3dda304fabffab1284619cdc9cdb
Zhang Jikun <39144313+zjk2606@users.noreply.github.com>
2021-11-02 03:13:41 +0800

    updated wrong sol that somehow ACed

--- Kattis/Dominos/solution.cpp
+++ Kattis/Dominos/solution.cpp
@@ -1,62 +1,55 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-using ll = long long;
-using pii = pair<int, int>;
-using vi = vector<int>;
-using vii = vector<pii>;
-#define fi first
-#define se second
-#define sz(c)  ((int)(c).size())
-#define all(c) (c).begin(), (c).end()
-#define forn(i,m, n)  for (int i = m, nnnn = (n); i < nnnn; ++i)
-#define pb  push_back
-#define mp  make_pair
 
-int main() {
+
+vector<bool> v;
+vector<vector<int>> adj;
+vector<bool> s;
+
+void dfs(int c, int origin){
+    v[c] = true;
+    for(int i : adj[c]){
+        if (!v[i]) dfs(i, origin);
+        else if (s[i] && i != origin) {
+            s[i] = false;
+        }
+    }
+}
+
+void solve(){
+    int n, m;
+    cin >> n >> m;
+    v = vector<bool>(n, false);
+    s = vector<bool>(n, true);
+    adj = vector<vector<int>>(n);
+    while(m--){
+        int a, b;
+        cin >> a >> b;
+        adj[a-1].push_back(b-1);
+        s[b-1] = false;
+    }
+    for(int i = 0;i < n; i++){
+        if (s[i]) {
+            dfs(i, i);
+        }
+    }
+    for(int i = 0;i < n; i++){
+        if (!v[i]) {
+            s[i] = true;
+            dfs(i, i);
+        }
+    }
+    int ans=0;
+    for(int i = 0;i < n; i++) ans+=s[i];
+    cout << ans<< '\n';
+
+}
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << setprecision(12) << fixed;
     int t;
     cin >> t;
-    forn(T, 0, t){
-        int n, m;
-        scanf("%d %d", &n, &m);;
-        int ufds[n], height[n];
-        bool visited[n], parent[n];
-        forn(i, 0, n) {
-            ufds[i] = i;
-            height[i] = 0;
-            parent[i] = false;
-        }
-        forn(i, 0, m){
-            int p, c;
-            scanf("%d %d", &p, &c);
-            p--;
-            c--;
-            parent[c] = true;
-            int p1 = p;
-            int p2 = c;
-            while (ufds[p1] != p1) p1 = ufds[p1];
-            while (ufds[p2] != p2) p2 = ufds[p2];
-            if (height[p1] > height[p2]){
-                ufds[p2] = p1;
-            } else if (height[p2] > height[p1]) {
-                ufds[p1] = p2;
-            } else {
-                ufds[p2] = p1;
-                height[p1]++;
-            }
-        }
-        forn(i, 0, n) visited[i] = false; 
-        int count = 0;
-        forn(i, 0, n){
-            if (parent[i] == false) {
-                count++;
-                int curr = ufds[i];
-                while (ufds[curr] != curr) curr = ufds[curr];
-                visited[curr] = true;
-            }
-        }
-        forn(i, 0, n){
-            if (ufds[i] == i && visited[i] == false) count++;
-        }
-        cout << count << '\n';
-    }
+    while(t--) solve();
 }
